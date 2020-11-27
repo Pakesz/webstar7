277500acc9cdd40afc8b558cd061d849eff36bee
Pakesz <norbi1119@gmail.com>
2020-11-27 16:40:31 +0100

    tetNelkul

--- TippOOP/src/tippoop/Jatek.java
+++ TippOOP/src/tippoop/Jatek.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class Jatek {
     // Első feladat
     public static void elso(List<Eredmeny> eredmenyek, Tipp t){
-        
         int szamlalo =0;
         for(Eredmeny e: eredmenyek){
             if(e.getSorszam().equals(t.getSorszam())){
@@ -23,6 +22,40 @@ public class Jatek {
         if(szamlalo == eredmenyek.size()){
             System.out.println("A tipp érvénytelen");
         }
-    
     }
+    
+    // 1es és 2as
+    
+    public static void tetNelkul(List<Eredmeny> eredmenyek, List<Tipp> tippek){
+        
+        int gyoztesTipp = 0;
+        int vesztesTipp = 0;
+        
+        for(Eredmeny e: eredmenyek){
+            for(Tipp t: tippek){
+                if(e.getSorszam().equals(t.getSorszam())){
+                    if(e.getKimenetel().equals(t.getKimenetel())){
+                       gyoztesTipp ++; 
+                    }
+                    else{
+                        vesztesTipp ++;
+                    }
+                }
+            }
+        }
+        
+        if((gyoztesTipp+vesztesTipp) != tippek.size()){
+            System.out.println("Érvénytelen szelvény");
+        }
+        else if(vesztesTipp > 0){
+            System.out.println("Vesztes szelvény");
+        }
+        else{
+            System.out.println("Gyöztes szelvény");
+        }
+        
+    }
+    
+    
+    // 3es és 4es
 }
--- TippOOP/src/tippoop/TippOOP.java
+++ TippOOP/src/tippoop/TippOOP.java
@@ -10,7 +10,6 @@ public class TippOOP {
     
     public static void main(String[] args) {
         List<Eredmeny> e = new ArrayList<Eredmeny>();
-        
         Eredmeny e1 = new Eredmeny("a001","Magyar-Angol","H",1.5);
         Eredmeny e2 = new Eredmeny("a002","Fradi-Ujpest","D",3.0);
         Eredmeny e3 = new Eredmeny("a003","Brazil-Argentin","V",2.0);
@@ -23,10 +22,18 @@ public class TippOOP {
         e.add(e4);
         e.add(e5);
         
+        List<Tipp> t = new ArrayList<Tipp>();
         Tipp t1 = new Tipp("a001","H");
+        Tipp t2 = new Tipp("a002","D");
+        Tipp t3 = new Tipp("a003","V");
         
+        t.add(t1);
+        t.add(t2);
+        t.add(t3);
         // t1.elso(e);
-        Jatek.elso(e, t1);
+        //Jatek.elso(e, t1);
+        
+        Jatek.tetNelkul(e, t);
     }
     
 }
